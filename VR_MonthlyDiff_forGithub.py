6fa119407a5a0337c7c6c823cd4643a8222742ce
rrlinrr <45771006+rrlinrr@users.noreply.github.com>
2024-03-21 11:49:15 -0400

    Update VR_MonthlyDiff_forGithub.py

--- VR_MonthlyDiff_forGithub.py
+++ VR_MonthlyDiff_forGithub.py
@@ -7,6 +7,8 @@ Created on Wed Mar 13 17:31:37 2024
 
 import pandas as pd
 import os
+import datetime
+from dateutil import rrule
 token = os.environ.get('NYS_APP_TOKEN')
 
 
@@ -14,6 +16,14 @@ token = os.environ.get('NYS_APP_TOKEN')
 regClassList = ['ARG', 	'AYG', 	'BOB', 	'CBS', 	'CCK', 	'CLG', 	'CME', 	'CMH', 	'EDU', 	'FPW', 	'GAC', 	'GSC', 	'GSM', 	'HAM', 	'HIS', 	'JCA', 	'JCL', 	'JSC', 	'JWV', 	'LOC', 	'LUA', 	'MCL', 	'MED', 	'NLM', 	'NYA', 	'NYC', 	'NYS', 	'ORG', 	'PAS', 	'PHS', 	'PPH', 	'RGL', 	'SCL', 	'SOS', 	'SPO', 	'SRF', 	'SRN', 	'STG', 	'SUP', 	'USC', 	'USS', 	'VPL', 	'WUG']
 
 boroughList = ["KINGS", "NEW YORK", "QUEENS" ,"BRONX", "RICHMOND"]
+
+start_date = datetime.datetime(2019, 12, 1)
+end_date = start
+monthList = []
+monthLabelList = []
+for dt in rrule.rrule(rrule.MONTHLY, dtstart=start_date, until=end_date):
+    monthList.append(calendar.month_name[dt.month] + " " + str(dt.year))
+monthLabelList = ['November 2019'] + monthList[:-1]
 
 rawdf = pd.read_csv("DMV_borough_full_excl_susp_rvct_output.csv")
 print("read output")
