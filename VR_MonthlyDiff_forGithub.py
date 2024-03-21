1a0c39cc4b6e0fb32275ba0b9ca4b5684c379073
rrlinrr <45771006+rrlinrr@users.noreply.github.com>
2024-03-21 10:54:18 -0400

    Update VR_MonthlyDiff_forGithub.py

--- VR_MonthlyDiff_forGithub.py
+++ VR_MonthlyDiff_forGithub.py
@@ -34,11 +34,12 @@ monthLabelList = ['November 2019'] + monthList[:-1]
 
 
 rawdf = pd.read_csv("DMV_borough_full_excl_susp_rvct_output.csv")
+print("read output")
 
 client = Socrata("data.ny.gov", token)
 results = client.get("w4pv-hbkt", limit=20000000)
 results_df = pd.DataFrame.from_records(results)
-
+print("read dmv")
 
 N = len(monthList) - 1
         
