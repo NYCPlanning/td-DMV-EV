755fbc467d1bbbf959c0e2734c7a508551d0e070
rrlinrr <45771006+rrlinrr@users.noreply.github.com>
2024-04-01 16:14:20 -0400

    Update VR_MonthlyDiff_forGithub.py

--- VR_MonthlyDiff_forGithub.py
+++ VR_MonthlyDiff_forGithub.py
@@ -65,3 +65,39 @@ rawdf.insert((len(monthList)-13)*3, monthLabelList[N]+"-EV Perc", [evbkdf.shape[
 
 rawdf.to_csv("DMV_borough_full_excl_susp_rvct_output.csv")
 print(rawdf)
+
+
+mergedf = rawdf[["County"]+ rawdf.columns[rawdf.columns.str.contains(pat = '-Total')].tolist()].melt(id_vars='County')
+mergedf['variable'] = mergedf['variable'].str.rstrip('-Total')
+mergedf = mergedf[mergedf["County"]!="Total"]
+#mergedf["HOVER"] ='<b>Month: </b>'+mergedf['variable']+'<br><b>County: </b>'+ mergedf['County']+'<br><b>Vehicle Registrations: </b>'+mergedf['value'].map('{:,.0f}'.format)
+
+linedf = rawdf[["County"]+ rawdf.columns[rawdf.columns.str.contains(pat = '-EV Perc')].tolist()].melt(id_vars='County')
+linedf['variable'] = linedf['variable'].str.rstrip('-EV Perc')
+linedf = linedf[linedf["County"]=="Total"]
+#linedf["HOVER"] ='<b>Month: </b>'+linedf['variable']+'<br><b>County: </b>'+ linedf['County']+'<br><b>Percent of EV Registrations: </b>'+linedf['value'].map('{:,.0f}'.format)
+
+mergedf.columns = ["County", "Month", "Vehicle Registrations"]
+linedf.columns = ["County", "Month", "Percent of EV Registrations"]
+
+fig1 = make_subplots(specs=[[{"secondary_y": True}]])
+mn = mergedf[mergedf["County"]=="NEW YORK"]
+fig1.add_trace(go.Bar(name='New York', x=mn['Month'], y=mn['Vehicle Registrations']))
+bk = mergedf[mergedf["County"]=="KINGS"]
+fig1.add_trace(go.Bar(name='Kings', x=bk['Month'], y=bk['Vehicle Registrations']))
+bx = mergedf[mergedf["County"]=="BRONX"]
+fig1.add_trace(go.Bar(name='Bronx', x=bx['Month'], y=bx['Vehicle Registrations']))
+qn = mergedf[mergedf["County"]=="QUEENS"]
+fig1.add_trace(go.Bar(name='Queens', x=qn['Month'], y=qn['Vehicle Registrations']))
+mn = mergedf[mergedf["County"]=="RICHMOND"]
+fig1.add_trace(go.Bar(name='Staten Island', x=mn['Month'], y=mn['Vehicle Registrations']))
+fig1.add_trace(go.Scatter(x=linedf["Month"], y=linedf["Percent of EV Registrations"], mode='lines', name = "Percent of EV Registrations"),secondary_y=True)
+#fig1.add_trace(go.Bar(name='DMV Standard Series Vehicle Registrations in NYC (Nov 2019 - Jan 2024) Excluding Revocation and Suspension', x=mergedf['variable'], y=mergedf['value'],  barmode = 'stack'),secondary_y=False)
+#fig1.add_trace(go.Bar(name='DMV Standard Series Vehicle Registrations in NYC (Nov 2019 - Jan 2024) Excluding Revocation and Suspension', x=mergedf['variable'], y=mergedf['value'], marker_color='#000080', hoverinfo='text', hovertext=mergedf['HOVER']),secondary_y=False)
+fig1.update_layout(barmode='stack',
+                  title = '<b>DMV Standard Series Vehicle and Electric Vehicle Registrations in NYC</b><br>Excluding Revocation and Suspension (November 2019 - '+ monthLabelList[N] +')').show()
+fig1.update_yaxes(title_text="Total Vehicle Registrations", secondary_y=False)
+fig1.update_yaxes(title_text="Percent of EV Registrations", secondary_y=True)
+fig1.update_xaxes(tickangle=45)
+#fig1.update_layout(hovermode="x")
+fig1.show()
