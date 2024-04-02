6cad4d0410641d00b8cf69034ec7c2564b5c39d0
rrlinrr <45771006+rrlinrr@users.noreply.github.com>
2024-04-02 14:50:14 -0400

    Update VR_MonthlyDiff_forGithub.py

--- VR_MonthlyDiff_forGithub.py
+++ VR_MonthlyDiff_forGithub.py
@@ -40,7 +40,7 @@ results = client.get("w4pv-hbkt", query=query)
 results_df = pd.DataFrame.from_records(results)
 
 
-N = len(monthList) - 2
+N = len(monthList) - 1
         
 vehdf = results_df.loc[results_df["registration_class"].isin(regClassList)]
     
